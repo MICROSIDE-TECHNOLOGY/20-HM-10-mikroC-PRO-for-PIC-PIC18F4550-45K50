b378fc73a8650cceff2159964a26e8663c342429
vhperaltaj <victor.peralta@microside.com>
2023-08-23 13:35:35 -0600

    Returned encoding to windows

--- Proyecto/BLE/45K50/SERIAL.c
+++ Proyecto/BLE/45K50/SERIAL.c
@@ -11,17 +11,17 @@ Driver Version    :  1.0
 ********************************************************************************/
 
 /*******************************************************************************
-Para usar el c√≥digo con bootloader, configurar como lo indica MICROSIDE:
+Para usar el cÛdigo con bootloader, configurar como lo indica MICROSIDE:
 1) Tools> Options> Output> Output Settings> Marcar "Long HEX format"
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
 #pragma orgall 0x1FFF                //Espacio reservado para bootloader
-#define LED_ON       LATA4_bit = 1   //Se declara una definici√≥n donde se asigna el pin de salida y el estado l√≥gico
-#define LED_OFF      LATA4_bit = 0   //Se declara una definici√≥n donde se asigna el pin de salida y el estado l√≥gico
-#define BOTON PORTA.f2               //Se declara una definici√≥n que contine la funcion que determian el estado de una entrada digital
+#define LED_ON       LATA4_bit = 1   //Se declara una definiciÛn donde se asigna el pin de salida y el estado lÛgico
+#define LED_OFF      LATA4_bit = 0   //Se declara una definiciÛn donde se asigna el pin de salida y el estado lÛgico
+#define BOTON PORTA.f2               //Se declara una definiciÛn que contine la funcion que determian el estado de una entrada digital
 
-void main() org 0x2000               //Se define el registro a partir del cual se almacenara el codigo l√≥gico
+void main() org 0x2000               //Se define el registro a partir del cual se almacenara el codigo lÛgico
 {
   ANSELC = 0;
   TRISB = 0x00;                      //Puerto B como salida
@@ -31,7 +31,7 @@ void main() org 0x2000               //Se define el registro a partir del cual s
   ANSA2_bit = 0;                     //Pin A2 como digital
   UART1_Init(9600);                  //Se asigna la velocidad del baudrate
   while(1){
-    if (UART1_Data_Ready() == 1) {   //Pregunta si ha recibido alg√∫n dato por el puerto serial
+    if (UART1_Data_Ready() == 1) {   //Pregunta si ha recibido alg˙n dato por el puerto serial
 
       char Caracter = UART1_Read();  //Guarda el caracter
 
@@ -44,10 +44,10 @@ void main() org 0x2000               //Se define el registro a partir del cual s
         UART1_Write_Text("LED ENCENDIDO");
       }
       else if (Caracter == '?') {
-        if (BOTON==1) {              //Pregunta el estado del bot√≥n
-          UART1_Write('0');          //Env√≠a un 0 si el bot√≥n no est√° presionado
+        if (BOTON==1) {              //Pregunta el estado del botÛn
+          UART1_Write('0');          //EnvÌa un 0 si el botÛn no est· presionado
         } else {
-          UART1_Write('1');          //Env√≠a un 1 si el bot√≥n est√° presionado
+          UART1_Write('1');          //EnvÌa un 1 si el botÛn est· presionado
         }
       }
     }
